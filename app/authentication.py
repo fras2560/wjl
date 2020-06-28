d6e1fc9d5687e9d558a25d7c2d86cfcc6add978e
Dallas Fraser <dfraser@digitaled.com>
2020-06-28 01:00:26 -0400

    Got facebook working as well.

--- app/authentication.py
+++ app/authentication.py
@@ -113,8 +113,6 @@ def get_user_info(blueprint: Blueprint) -> UserInfo:
     resp = None
     if blueprint.name == FACEBOOK:
         resp = blueprint.session.get("/me?fields=id,email")
-        print(resp)
-        print(resp.json())
     elif blueprint.name == GOOGLE:
         resp = blueprint.session.get("/oauth2/v1/userinfo")
     elif blueprint.name == GITHUB:
