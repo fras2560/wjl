7dedaf8dc627a44d42909df1d7d742d865961260
Dallas Fraser <dfraser@digitaled.com>
2020-07-06 00:08:36 -0400

    Missed a previous route

--- app/views/schedule.py
+++ app/views/schedule.py
@@ -6,6 +6,7 @@ from app.model import Session, Match
 from app.views.helper import get_active_session, get_base_data
 from app.views.types import ScheduleRecord
 from app.logging import LOGGER
+from app.errors import NotFoundException
 from datetime import datetime
 import json
 
@@ -37,3 +38,15 @@ def schedule():
                            league_sessions=league_sessions,
                            active_session=active_session,
                            today=datetime.now().strftime("%Y-%m-%d"))
+
+
+@wjl_app.route("/match_results/<int:match_id>")
+def match_result(match_id: int):
+    match = Match.query.get(match_id)
+    if match is None:
+        raise NotFoundException(f"Sorry, match not found - {match_id}")
+    sheets = [sheet.json() for sheet in match.sheets]
+    return render_template("match_results.html",
+                           base_data=get_base_data(),
+                           match=match.json(),
+                           sheets=sheets)
