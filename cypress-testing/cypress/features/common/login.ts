4b84fdd404b14750b047b96ae67cc781b75c849e
Dallas Fraser <dfraser@digitaled.com>
2020-07-10 10:27:18 -0400

    On login function re-visit the homepage

--- cypress-testing/cypress/features/common/login.ts
+++ cypress-testing/cypress/features/common/login.ts
@@ -1,4 +1,4 @@
-import { Given } from 'cypress-cucumber-preprocessor/steps';
+import { Given, When } from 'cypress-cucumber-preprocessor/steps';
 import { Player } from '@Models/player';
 import uuidv4 from 'uuid/v4';
 
@@ -26,10 +26,20 @@ export function randomEmail(): string {
     return randomName() + '-' + randomName() + '@wjl.ca';
 }
 
+/** A function that logs in the user. */
 const login = (): void => {
     const player = new Player(randomEmail(), randomName());
     cy.login(player);
     cy.wrap(player).as('current_player');
+    cy.visit('');
 };
 
 Given('I am logged in', login);
+When('I login', login);
+
+/** A unfciton the guarantees currently logged out */
+const loggedOut = (): void => {
+    cy.logout();
+};
+
+Given('I am not logged in', loggedOut);
